c7bc911cfd382e40020a7fad74c527db1cd93478
yokonut <koyohan730@gmail.com>
2024-10-07 23:59:42 +0800

    finished sorting.h and .c

--- sort.c
+++ sort.c
@@ -1,11 +1,37 @@
 #include "sort.h"
 
-
 int main()
 {
-    int data[] = {20, 12, 10, 15, 2};
-    int size = sizeof(data) / sizeof(data[0]);
-    selectionSort(data, size);
-    printf("Sorted array in Acsending Order:\n");
-    printArray(data, size);
+    struct point data[STACK_LENGTH];
+
+    struct point arr[STACK_LENGTH] =
+    {
+        {4.4, 8.8},
+        {3.3, 6.6},
+        {9.9, 19.8},
+        {2.2, 4.4},
+        {0.0, 0.0},
+        {8.8, 17.6},
+        {5.5, 11.0},
+        {6.6, 13.2},
+        {1.1, 2.2},
+        {7.7, 15.4}
+    };
+
+    /*
+        srand(time(NULL));
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i].x = ((float)rand() / RAND_MAX) * 100; // Random float between 0 and 100
+            arr[i].y = ((float)rand() / RAND_MAX) * 100; // Random float between 0 and 100
+        }
+    */
+
+    selectionSort(arr,10);
+
+   // mergesort(arr,10);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Point %d: (%.2f, %.2f)\n", i, arr[i].x, arr[i].y);
+    }
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 // Selection sort in C
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define STACK_LENGTH 32768
 
 struct point
@@ -11,6 +13,12 @@ struct point
 
 struct point arr[STACK_LENGTH];
 
+void swap(struct point *a, struct point *b);
+void selectionSort(struct point arr[], int size);
+void mergesort(struct point arr[], int size);
+void merge_sort_recursion(struct point arr[], int l, int r);
+void merge_sorted_array(struct point arr[], int l, int m, int r);
+
 void swap(struct point *a, struct point *b)
 {
     struct point temp = *a;
@@ -20,25 +28,64 @@ void swap(struct point *a, struct point *b)
 
 void selectionSort(struct point arr[], int size)
 {
-
     for (int j = 0; j < size - 1; j++)
     {
-        int min_idx;
-        min_idx = j;
-        for (int i = j + 1; j < size; i++)
+        int min_idx = j;
+        for (int i = j + 1; i < size; i++)
         {
-
-            if (arr[i].y < arr[min_idx].y)
-                min_idx = i;
-            else if (arr[i].y == arr[min_idx].y)
+            if (arr[i].y < arr[min_idx].y || (arr[i].y == arr[min_idx].y && arr[i].x < arr[min_idx].x))
             {
-                if (arr[i].x < arr[min_idx].x)
-                {
-                    min_idx = i;
-                }
+                min_idx = i;
             }
         }
-
         swap(&arr[min_idx], &arr[j]);
     }
+}
+void mergesort(struct point arr[], int size)
+{
+    merge_sort_recursion(arr, 0, size - 1);
+}
+
+void merge_sort_recursion(struct point arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        merge_sort_recursion(arr, l, m);
+        merge_sort_recursion(arr, m + 1, r);
+        merge_sorted_array(arr, l, m, r);
+    }
+}
+
+void merge_sorted_array(struct point arr[], int l, int m, int r)
+{
+
+    int left_length = m - l + 1;
+    int right_length = r - m;
+
+    struct point temp_left[left_length], temp_right[right_length];
+
+    for (int i = 0; i < left_length; i++)
+        temp_left[i] = arr[l + i];
+    for (int i = 0; i < right_length; i++)
+        temp_right[i] = arr[m + 1 + i];
+
+    int i = 0, j = 0, k = l;
+    while (i < left_length && j < right_length)
+    {
+        if (temp_left[i].y < temp_right[j].y ||
+            (temp_left[i].y == temp_right[j].y && temp_left[i].x <= temp_right[j].x))
+        {
+            arr[k++] = temp_left[i++];
+        }
+        else
+        {
+            arr[k++] = temp_right[j++];
+        }
+    }
+
+    while (i < left_length)
+        arr[k++] = temp_left[i++];
+    while (j < right_length)
+        arr[k++] = temp_right[j++];
 }
